d56fea52395c2cadccfcd72e21b90d4566de3c53
ali <ali@swishtag.com>
2023-07-01 17:39:26 +0500

    start work on admin panel

---
+++ admin/index.php
@@ -0,0 +1,438 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="description" content="Neon Admin Panel" />
+	<meta name="author" content="" />
+
+	<link rel="icon" href="assets/images/favicon.ico">
+
+	<title>Neon | Dashboard</title>
+
+	<link rel="stylesheet" href="assets/js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css">
+	<link rel="stylesheet" href="assets/css/font-icons/entypo/css/entypo.css">
+	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic">
+	<link rel="stylesheet" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" href="assets/css/neon-core.css">
+	<link rel="stylesheet" href="assets/css/neon-theme.css">
+	<link rel="stylesheet" href="assets/css/neon-forms.css">
+	<link rel="stylesheet" href="assets/css/custom.css">
+
+	<script src="assets/js/jquery-1.11.3.min.js"></script>
+
+	<!--[if lt IE 9]><script src="assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+	
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+
+</head>
+<body class="page-body  page-fade" data-url="http://neon.dev">
+
+<div class="page-container"><!-- add class "sidebar-collapsed" to close sidebar by default, "chat-visible" to make chat appear always -->
+	
+	<div class="sidebar-menu">
+
+		<div class="sidebar-menu-inner">
+			
+			<header class="logo-env">
+
+				<!-- logo -->
+				<div class="logo">
+					<a href="index.html">
+						<img src="assets/images/logo@2x.png" width="120" alt="" />
+					</a>
+				</div>
+
+				<!-- logo collapse icon -->
+				<div class="sidebar-collapse">
+					<a href="#" class="sidebar-collapse-icon"><!-- add class "with-animation" if you want sidebar to have animation during expanding/collapsing transition -->
+						<i class="entypo-menu"></i>
+					</a>
+				</div>
+
+								
+				<!-- open/close menu icon (do not remove if you want to enable menu on mobile devices) -->
+				<div class="sidebar-mobile-menu visible-xs">
+					<a href="#" class="with-animation"><!-- add class "with-animation" to support animation -->
+						<i class="entypo-menu"></i>
+					</a>
+				</div>
+
+			</header>
+			
+									
+			<ul id="main-menu" class="main-menu">
+				<!-- add class "multiple-expanded" to allow multiple submenus to open -->
+				<!-- class "auto-inherit-active-class" will automatically add "active" class for parent elements who are marked already with class "active" -->
+				<li class="active opened active has-sub">
+					<a href="index.html">
+						<span class="title">Dashboard</span>
+					</a>
+				</li>
+                	<li class="active opened has-sub">
+					<a href="leaves.html">
+						
+						<span class="title">Leaves</span>
+					</a>
+				</li>
+                	<li class="active opened has-sub">
+					<a href="personal.html">
+						
+						<span class="title">Personal</span>
+					</a>
+				</li>
+                
+
+			</ul>
+			
+		</div>
+
+	</div>
+
+	<div class="main-content">
+				
+		<div class="row">
+		
+			<!-- Profile Info and Notifications -->
+			<div class="col-md-6 col-sm-8 clearfix">
+		
+				<ul class="user-info pull-left pull-none-xsm">
+		
+					<!-- Profile Info -->
+					<li class="profile-info dropdown"><!-- add class "pull-right" if you want to place this from right -->
+		
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+							<img src="assets/images/thumb-1@2x.png" alt="" class="img-circle" width="44" />
+							John Henderson
+						</a>
+					
+		
+				</ul>
+		
+			</div>
+		
+		
+			<!-- Raw Links -->
+			<div class="col-md-6 col-sm-4 clearfix hidden-xs">
+		
+				<ul class="list-inline links-list pull-right">
+		
+					<li class="sep"></li>
+		
+					<li>
+						<a href="extra-login.html">
+							Log Out <i class="entypo-logout right"></i>
+						</a>
+					</li>
+				</ul>
+		
+			</div>
+		
+		</div>
+		
+		<hr />
+		
+		
+		<script type="text/javascript">
+		
+		
+		
+		function getRandomInt(min, max)
+		{
+			return Math.floor(Math.random() * (max - min + 1)) + min;
+		}
+		</script>
+		
+		
+		<div class="row">
+			<div class="col-sm-3 col-xs-6">
+		
+				<div class="tile-stats tile-red">
+					<div class="icon"><i class="entypo-users"></i></div>
+					<div class="num" data-start="0" data-end="83" data-postfix="" data-duration="1500" data-delay="0">0</div>
+		
+					<h3>Approved Leaves</h3>
+					<p>All Approved Leaves</p>
+				</div>
+		
+			</div>
+		
+			<div class="col-sm-3 col-xs-6">
+		
+				<div class="tile-stats tile-green">
+					<div class="icon"><i class="entypo-chart-bar"></i></div>
+					<div class="num" data-start="0" data-end="135" data-postfix="" data-duration="1500" data-delay="600">0</div>
+		
+					<h3>Pending Leave</h3>
+					<p>Pending leave</p>
+				</div>
+		
+			</div>
+			
+			<div class="clear visible-xs"></div>
+		
+		
+
+		</div>
+		
+		<br />
+		
+		
+		<br />
+		
+		
+		<script type="text/javascript">
+			// Code used to add Todo Tasks
+			jQuery(document).ready(function($)
+			{
+				var $todo_tasks = $("#todo_tasks");
+		
+				$todo_tasks.find('input[type="text"]').on('keydown', function(ev)
+				{
+					if(ev.keyCode == 13)
+					{
+						ev.preventDefault();
+		
+						if($.trim($(this).val()).length)
+						{
+							var $todo_entry = $('<li><div class="checkbox checkbox-replace color-white"><input type="checkbox" /><label>'+$(this).val()+'</label></div></li>');
+							$(this).val('');
+		
+							$todo_entry.appendTo($todo_tasks.find('.todo-list'));
+							$todo_entry.hide().slideDown('fast');
+							replaceCheckboxes();
+						}
+					}
+				});
+			});
+		</script>
+		
+	</div>
+
+		
+	<div id="chat" class="fixed" data-current-user="Art Ramadani" data-order-by-status="1" data-max-chat-history="25">
+	
+		<div class="chat-inner">
+	
+	
+			<h2 class="chat-header">
+				<a href="#" class="chat-close"><i class="entypo-cancel"></i></a>
+	
+				<i class="entypo-users"></i>
+				Chat
+				<span class="badge badge-success is-hidden">0</span>
+			</h2>
+	
+	
+			<div class="chat-group" id="group-1">
+				<strong>Favorites</strong>
+	
+				<a href="#" id="sample-user-123" data-conversation-history="#sample_history"><span class="user-status is-online"></span> <em>Catherine J. Watkins</em></a>
+				<a href="#"><span class="user-status is-online"></span> <em>Nicholas R. Walker</em></a>
+				<a href="#"><span class="user-status is-busy"></span> <em>Susan J. Best</em></a>
+				<a href="#"><span class="user-status is-offline"></span> <em>Brandon S. Young</em></a>
+				<a href="#"><span class="user-status is-idle"></span> <em>Fernando G. Olson</em></a>
+			</div>
+	
+	
+			<div class="chat-group" id="group-2">
+				<strong>Work</strong>
+	
+				<a href="#"><span class="user-status is-offline"></span> <em>Robert J. Garcia</em></a>
+				<a href="#" data-conversation-history="#sample_history_2"><span class="user-status is-offline"></span> <em>Daniel A. Pena</em></a>
+				<a href="#"><span class="user-status is-busy"></span> <em>Rodrigo E. Lozano</em></a>
+			</div>
+	
+	
+			<div class="chat-group" id="group-3">
+				<strong>Social</strong>
+	
+				<a href="#"><span class="user-status is-busy"></span> <em>Velma G. Pearson</em></a>
+				<a href="#"><span class="user-status is-offline"></span> <em>Margaret R. Dedmon</em></a>
+				<a href="#"><span class="user-status is-online"></span> <em>Kathleen M. Canales</em></a>
+				<a href="#"><span class="user-status is-offline"></span> <em>Tracy J. Rodriguez</em></a>
+			</div>
+	
+		</div>
+	
+		<!-- conversation template -->
+		<div class="chat-conversation">
+	
+			<div class="conversation-header">
+				<a href="#" class="conversation-close"><i class="entypo-cancel"></i></a>
+	
+				<span class="user-status"></span>
+				<span class="display-name"></span>
+				<small></small>
+			</div>
+	
+			<ul class="conversation-body">
+			</ul>
+	
+			<div class="chat-textarea">
+				<textarea class="form-control autogrow" placeholder="Type your message"></textarea>
+			</div>
+	
+		</div>
+	
+	</div>
+	
+	
+	<!-- Chat Histories -->
+	<ul class="chat-history" id="sample_history">
+		<li>
+			<span class="user">Art Ramadani</span>
+			<p>Are you here?</p>
+			<span class="time">09:00</span>
+		</li>
+	
+		<li class="opponent">
+			<span class="user">Catherine J. Watkins</span>
+			<p>This message is pre-queued.</p>
+			<span class="time">09:25</span>
+		</li>
+	
+		<li class="opponent">
+			<span class="user">Catherine J. Watkins</span>
+			<p>Whohoo!</p>
+			<span class="time">09:26</span>
+		</li>
+	
+		<li class="opponent unread">
+			<span class="user">Catherine J. Watkins</span>
+			<p>Do you like it?</p>
+			<span class="time">09:27</span>
+		</li>
+	</ul>
+	
+	
+	
+	
+	<!-- Chat Histories -->
+	<ul class="chat-history" id="sample_history_2">
+		<li class="opponent unread">
+			<span class="user">Daniel A. Pena</span>
+			<p>I am going out.</p>
+			<span class="time">08:21</span>
+		</li>
+	
+		<li class="opponent unread">
+			<span class="user">Daniel A. Pena</span>
+			<p>Call me when you see this message.</p>
+			<span class="time">08:27</span>
+		</li>
+	</ul>
+
+	
+</div>
+
+	<!-- Sample Modal (Default skin) -->
+	<div class="modal fade" id="sample-modal-dialog-1">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h4 class="modal-title">Widget Options - Default Modal</h4>
+				</div>
+				
+				<div class="modal-body">
+					<p>Now residence dashwoods she excellent you. Shade being under his bed her. Much read on as draw. Blessing for ignorant exercise any yourself unpacked. Pleasant horrible but confined day end marriage. Eagerness furniture set preserved far recommend. Did even but nor are most gave hope. Secure active living depend son repair day ladies now.</p>
+				</div>
+				
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					<button type="button" class="btn btn-primary">Save changes</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<!-- Sample Modal (Skin inverted) -->
+	<div class="modal invert fade" id="sample-modal-dialog-2">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h4 class="modal-title">Widget Options - Inverted Skin Modal</h4>
+				</div>
+				
+				<div class="modal-body">
+					<p>Now residence dashwoods she excellent you. Shade being under his bed her. Much read on as draw. Blessing for ignorant exercise any yourself unpacked. Pleasant horrible but confined day end marriage. Eagerness furniture set preserved far recommend. Did even but nor are most gave hope. Secure active living depend son repair day ladies now.</p>
+				</div>
+				
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					<button type="button" class="btn btn-primary">Save changes</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<!-- Sample Modal (Skin gray) -->
+	<div class="modal gray fade" id="sample-modal-dialog-3">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h4 class="modal-title">Widget Options - Gray Skin Modal</h4>
+				</div>
+				
+				<div class="modal-body">
+					<p>Now residence dashwoods she excellent you. Shade being under his bed her. Much read on as draw. Blessing for ignorant exercise any yourself unpacked. Pleasant horrible but confined day end marriage. Eagerness furniture set preserved far recommend. Did even but nor are most gave hope. Secure active living depend son repair day ladies now.</p>
+				</div>
+				
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					<button type="button" class="btn btn-primary">Save changes</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+	<!-- Imported styles on this page -->
+	<link rel="stylesheet" href="assets/js/jvectormap/jquery-jvectormap-1.2.2.css">
+	<link rel="stylesheet" href="assets/js/rickshaw/rickshaw.min.css">
+
+	<!-- Bottom scripts (common) -->
+	<script src="assets/js/gsap/TweenMax.min.js"></script>
+	<script src="assets/js/jquery-ui/js/jquery-ui-1.10.3.minimal.min.js"></script>
+	<script src="assets/js/bootstrap.js"></script>
+	<script src="assets/js/joinable.js"></script>
+	<script src="assets/js/resizeable.js"></script>
+	<script src="assets/js/neon-api.js"></script>
+	<script src="assets/js/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+
+
+	<!-- Imported scripts on this page -->
+	<script src="assets/js/jvectormap/jquery-jvectormap-europe-merc-en.js"></script>
+	<script src="assets/js/jquery.sparkline.min.js"></script>
+	<script src="assets/js/rickshaw/vendor/d3.v3.js"></script>
+	<script src="assets/js/rickshaw/rickshaw.min.js"></script>
+	<script src="assets/js/raphael-min.js"></script>
+	<script src="assets/js/morris.min.js"></script>
+	<script src="assets/js/toastr.js"></script>
+	<script src="assets/js/neon-chat.js"></script>
+
+
+	<!-- JavaScripts initializations and stuff -->
+	<script src="assets/js/neon-custom.js"></script>
+
+
+	<!-- Demo Settings -->
+	<script src="assets/js/neon-demo.js"></script>
+
+</body>
+</html>
