4752d2653a2fc94d25df51d748bf5db28a576258
Ali <syedaliahmed004@gmail.com>
2023-07-09 16:11:25 +0500

    working on admin panel update/attendecne

--- admin/attendance-record.php
+++ admin/attendance-record.php
@@ -1,143 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content="Neon Admin Panel" />
-    <meta name="author" content="" />
-
-    <link rel="icon" href="assets/images/favicon.ico">
-
-    <title>Neon | Dashboard</title>
-
-    <link rel="stylesheet" href="assets/js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css">
-    <link rel="stylesheet" href="assets/css/font-icons/entypo/css/entypo.css">
-    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic">
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
-    <link rel="stylesheet" href="assets/css/neon-core.css">
-    <link rel="stylesheet" href="assets/css/neon-theme.css">
-    <link rel="stylesheet" href="assets/css/neon-forms.css">
-    <link rel="stylesheet" href="assets/css/custom.css">
-
-    <script src="assets/js/jquery-1.11.3.min.js"></script>
-
-    <!--[if lt IE 9]><script src="assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-
-
-</head>
+<?php include('./includes/header.php'); ?>
+<?php
+$id = null;
+if (isset($_GET['id']) && $_GET['id'] !== '') {
+    $id = $_GET['id'];
+} else {
+    echo '<h1>Page Not found</h1>';
+    die();
+}
+function query($conn, $sql)
+{
+    $row1 = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($row1) <= 0) {
+        return [];
+    }
+    $record1 = mysqli_fetch_assoc($row1);
+    return $record1;
+}
+include('../connection.php');
+?>
 
 <body class="page-body  page-fade" data-url="http://neon.dev">
 
     <div class="page-container"><!-- add class "sidebar-collapsed" to close sidebar by default, "chat-visible" to make chat appear always -->
+        <?php include('./includes/sidebar.php') ?>
 
-        <div class="sidebar-menu">
-
-            <div class="sidebar-menu-inner">
-
-                <header class="logo-env">
-
-                    <!-- logo -->
-                    <div class="logo">
-                        <a href="index.html">
-                            <img src="assets/images/logo@2x.png" width="120" alt="" />
-                        </a>
-                    </div>
-
-                    <!-- logo collapse icon -->
-                    <div class="sidebar-collapse">
-                        <a href="#" class="sidebar-collapse-icon"><!-- add class "with-animation" if you want sidebar to have animation during expanding/collapsing transition -->
-                            <i class="entypo-menu"></i>
-                        </a>
-                    </div>
-
-
-                    <!-- open/close menu icon (do not remove if you want to enable menu on mobile devices) -->
-                    <div class="sidebar-mobile-menu visible-xs">
-                        <a href="#" class="with-animation"><!-- add class "with-animation" to support animation -->
-                            <i class="entypo-menu"></i>
-                        </a>
-                    </div>
-
-                </header>
-
-
-                <ul id="main-menu" class="main-menu">
-                    <!-- add class "multiple-expanded" to allow multiple submenus to open -->
-                    <!-- class "auto-inherit-active-class" will automatically add "active" class for parent elements who are marked already with class "active" -->
-                    <li class="active opened active has-sub">
-                        <a href="index.html">
-                            <span class="title">Dashboard</span>
-                        </a>
-                    </li>
-                    <li class="active opened has-sub">
-                        <a href="leaves.html">
-
-                            <span class="title">Leaves</span>
-                        </a>
-                    </li>
-                    <li class="active opened has-sub">
-                        <a href="personal.html">
-
-                            <span class="title">Personal</span>
-                        </a>
-                    </li>
-
-
-                </ul>
-
-            </div>
-
-        </div>
 
         <div class="main-content">
-
-            <div class="row">
-
-                <!-- Profile Info and Notifications -->
-                <div class="col-md-6 col-sm-8 clearfix">
-
-                    <ul class="user-info pull-left pull-none-xsm">
-
-                        <!-- Profile Info -->
-                        <li class="profile-info dropdown"><!-- add class "pull-right" if you want to place this from right -->
-
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <img src="assets/images/thumb-1@2x.png" alt="" class="img-circle" width="44" />
-                                John Henderson
-                            </a>
-
-
-                    </ul>
-
-                </div>
-
-
-                <!-- Raw Links -->
-                <div class="col-md-6 col-sm-4 clearfix hidden-xs">
-
-                    <ul class="list-inline links-list pull-right">
-
-                        <li class="sep"></li>
-
-                        <li>
-                            <a href="extra-login.html">
-                                Log Out <i class="entypo-logout right"></i>
-                            </a>
-                        </li>
-                    </ul>
-
-                </div>
-
-            </div>
+            <?php include('./includes/top-navbar.php') ?>
 
             <hr />
 
@@ -151,27 +40,53 @@
 
             <div class="row">
                 <div class="panel-body">
-
+                    <?php
+                    if ($id) {
+                        $sql = "SELECT * FROM users JOIN attendences ON users.id = attendences.user_id where users.id = '$id'";
+                        $user = query($conn, $sql);
+                    }
+                    ?>
                     <div class="col-sm-3 col-xs-12 col-lg-12">
-                        <h3>abc@gmail.com attendance records</h3>
+                        <?php
+                        if ($id && $user) {
+                        ?>
+                            <h3><?php echo $user['email']; ?> leave records</h3>
+                        <?php } ?>
+
 
                         <div class="row">
                             <div class="col-md-12">
 
                                 <table class="table table-bordered responsive">
+
                                     <thead>
                                         <tr>
                                             <th width="15%">#</th>
-                                            <th>Checkin time</th>
-                                            <th>Checkout time</th>
+                                            <th>Check-in Time</th>
+                                            <th>Check-out Time</th>
+                                    
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <td>03</td>
-                                            <td>1 MAY</td>
-                                            <td>2 MAY</td>
-                                        </tr>
+                                        <?php
+                                        $i = 1;
+
+                                        $row1 = mysqli_query($conn, $sql);
+                                        if (mysqli_num_rows($row1) > 0) {
+
+                                            while ($record = mysqli_fetch_assoc($row1)) {
+                                        ?>
+                                                <tr>
+                                                    <td><?php echo $i; ?></td>
+                                                    <td><?php echo $record['check_in']; ?></td>
+                                                    <td><?php echo $record['check_out']; ?></td>
+                                                    
+                                                </tr>
+                                        <?php $i++;
+                                            }
+                                        }
+                                        ?>
+
                                     </tbody>
                                 </table>
 
@@ -399,39 +314,4 @@
 
 
 
-
-    <!-- Imported styles on this page -->
-    <link rel="stylesheet" href="assets/js/jvectormap/jquery-jvectormap-1.2.2.css">
-    <link rel="stylesheet" href="assets/js/rickshaw/rickshaw.min.css">
-
-    <!-- Bottom scripts (common) -->
-    <script src="assets/js/gsap/TweenMax.min.js"></script>
-    <script src="assets/js/jquery-ui/js/jquery-ui-1.10.3.minimal.min.js"></script>
-    <script src="assets/js/bootstrap.js"></script>
-    <script src="assets/js/joinable.js"></script>
-    <script src="assets/js/resizeable.js"></script>
-    <script src="assets/js/neon-api.js"></script>
-    <script src="assets/js/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-
-
-    <!-- Imported scripts on this page -->
-    <script src="assets/js/jvectormap/jquery-jvectormap-europe-merc-en.js"></script>
-    <script src="assets/js/jquery.sparkline.min.js"></script>
-    <script src="assets/js/rickshaw/vendor/d3.v3.js"></script>
-    <script src="assets/js/rickshaw/rickshaw.min.js"></script>
-    <script src="assets/js/raphael-min.js"></script>
-    <script src="assets/js/morris.min.js"></script>
-    <script src="assets/js/toastr.js"></script>
-    <script src="assets/js/neon-chat.js"></script>
-
-
-    <!-- JavaScripts initializations and stuff -->
-    <script src="assets/js/neon-custom.js"></script>
-
-
-    <!-- Demo Settings -->
-    <script src="assets/js/neon-demo.js"></script>
-
-</body>
-
-</html>
+<?php include('includes/footer.php'); ?>
--- admin/leave-record.php
+++ admin/leave-record.php
@@ -1,143 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content="Neon Admin Panel" />
-    <meta name="author" content="" />
-
-    <link rel="icon" href="assets/images/favicon.ico">
-
-    <title>Neon | Dashboard</title>
-
-    <link rel="stylesheet" href="assets/js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css">
-    <link rel="stylesheet" href="assets/css/font-icons/entypo/css/entypo.css">
-    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic">
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
-    <link rel="stylesheet" href="assets/css/neon-core.css">
-    <link rel="stylesheet" href="assets/css/neon-theme.css">
-    <link rel="stylesheet" href="assets/css/neon-forms.css">
-    <link rel="stylesheet" href="assets/css/custom.css">
-
-    <script src="assets/js/jquery-1.11.3.min.js"></script>
-
-    <!--[if lt IE 9]><script src="assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-
-
-</head>
+<?php include('./includes/header.php'); ?>
+<?php
+$id = null;
+if(isset($_GET['id']) && $_GET['id']!==''){
+  $id = $_GET['id'];
+}else {
+    echo '<h1>Page Not found</h1>';
+    die();
+}
+function query($conn, $sql)
+{
+    $row1 = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($row1) <= 0) {
+        return [];
+    }
+    $record1 = mysqli_fetch_assoc($row1);
+    return $record1;
+}
+include('../connection.php');
+?>
 
 <body class="page-body  page-fade" data-url="http://neon.dev">
 
     <div class="page-container"><!-- add class "sidebar-collapsed" to close sidebar by default, "chat-visible" to make chat appear always -->
+        <?php include('./includes/sidebar.php') ?>
 
-        <div class="sidebar-menu">
-
-            <div class="sidebar-menu-inner">
-
-                <header class="logo-env">
-
-                    <!-- logo -->
-                    <div class="logo">
-                        <a href="index.html">
-                            <img src="assets/images/logo@2x.png" width="120" alt="" />
-                        </a>
-                    </div>
-
-                    <!-- logo collapse icon -->
-                    <div class="sidebar-collapse">
-                        <a href="#" class="sidebar-collapse-icon"><!-- add class "with-animation" if you want sidebar to have animation during expanding/collapsing transition -->
-                            <i class="entypo-menu"></i>
-                        </a>
-                    </div>
-
-
-                    <!-- open/close menu icon (do not remove if you want to enable menu on mobile devices) -->
-                    <div class="sidebar-mobile-menu visible-xs">
-                        <a href="#" class="with-animation"><!-- add class "with-animation" to support animation -->
-                            <i class="entypo-menu"></i>
-                        </a>
-                    </div>
-
-                </header>
-
-
-                <ul id="main-menu" class="main-menu">
-                    <!-- add class "multiple-expanded" to allow multiple submenus to open -->
-                    <!-- class "auto-inherit-active-class" will automatically add "active" class for parent elements who are marked already with class "active" -->
-                    <li class="active opened active has-sub">
-                        <a href="index.html">
-                            <span class="title">Dashboard</span>
-                        </a>
-                    </li>
-                    <li class="active opened has-sub">
-                        <a href="leaves.html">
-
-                            <span class="title">Leaves</span>
-                        </a>
-                    </li>
-                    <li class="active opened has-sub">
-                        <a href="personal.html">
-
-                            <span class="title">Personal</span>
-                        </a>
-                    </li>
-
-
-                </ul>
-
-            </div>
-
-        </div>
 
         <div class="main-content">
-
-            <div class="row">
-
-                <!-- Profile Info and Notifications -->
-                <div class="col-md-6 col-sm-8 clearfix">
-
-                    <ul class="user-info pull-left pull-none-xsm">
-
-                        <!-- Profile Info -->
-                        <li class="profile-info dropdown"><!-- add class "pull-right" if you want to place this from right -->
-
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <img src="assets/images/thumb-1@2x.png" alt="" class="img-circle" width="44" />
-                                John Henderson
-                            </a>
-
-
-                    </ul>
-
-                </div>
-
-
-                <!-- Raw Links -->
-                <div class="col-md-6 col-sm-4 clearfix hidden-xs">
-
-                    <ul class="list-inline links-list pull-right">
-
-                        <li class="sep"></li>
-
-                        <li>
-                            <a href="extra-login.html">
-                                Log Out <i class="entypo-logout right"></i>
-                            </a>
-                        </li>
-                    </ul>
-
-                </div>
-
-            </div>
+            <?php include('./includes/top-navbar.php') ?>
 
             <hr />
 
@@ -151,14 +40,25 @@
 
             <div class="row">
                 <div class="panel-body">
-
+                    <?php
+                    if($id){
+                    $sql = "SELECT * FROM users JOIN employee_leaves ON users.id = employee_leaves.user_id where users.id = '$id'";
+                    $user = query($conn, $sql);
+                    }
+                    ?>
                     <div class="col-sm-3 col-xs-12 col-lg-12">
-                        <h3>abc@gmail.com leave records</h3>
+                        <?php
+                        if($id && $user){
+                        ?>
+                        <h3><?php echo $user['email']; ?> leave records</h3>
+                        <?php } ?>
+
 
                         <div class="row">
                             <div class="col-md-12">
-
+      
                                 <table class="table table-bordered responsive">
+                                    
                                     <thead>
                                         <tr>
                                             <th width="15%">#</th>
@@ -169,16 +69,38 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <td>03</td>
-                                            <td>1 MAY</td>
-                                            <td>2 MAY</td>
-                                            <td>Sex leave</td>
-                                            <td>
-                                                <div class="label label-secondary">Rejected</div>
-                                                <div class="label label-success">Accepted </div>
-                                            </td>
-                                        </tr>
+                                        <?php
+                                        $i = 1;
+                                        
+                                        $row1 = mysqli_query($conn, $sql);
+                                        if (mysqli_num_rows($row1) > 0) {
+                                            
+                                            while ($record = mysqli_fetch_assoc($row1)) {
+                                        ?>
+                                                <tr>
+                                                    <td><?php echo $i; ?></td>
+                                                    <td><?php echo $record['start_date']; ?></td>
+                                                    <td><?php echo $record['end_date']; ?></td>
+                                                    <td><?php echo $record['leave_type']; ?></td>
+                                                    <td>
+                                                        <?php if ($record['status'] == 'Approved') {
+                                                        ?>
+                                                            <div class="label label-success">Approved </div>
+
+                                                        <?php
+                                                        } else if ($record['status'] == 'Rejected') {
+                                                        ?>
+                                                            <div class="label label-secondary">Rejected</div>
+
+                                                        <?php
+                                                        } ?>
+                                                    </td>
+                                                </tr>
+                                        <?php $i++;
+                                            }
+                                        }
+                                        ?>
+
                                     </tbody>
                                 </table>
 
