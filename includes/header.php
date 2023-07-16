d7e24dcdff3dcb2e78246a277882f1c48cd2add2
Ali <syedaliahmed004@gmail.com>
2023-07-16 23:46:04 +0500

    working on employe side

---
+++ includes/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Neon Admin Panel" />
+    <meta name="author" content="" />
+
+    <link rel="icon" href="assets/images/favicon.ico">
+
+    <title>Neon | Dashboard</title>
+
+    <link rel="stylesheet" href="assets/js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css">
+    <link rel="stylesheet" href="assets/css/font-icons/entypo/css/entypo.css">
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/neon-core.css">
+    <link rel="stylesheet" href="assets/css/neon-theme.css">
+    <link rel="stylesheet" href="assets/css/neon-forms.css">
+    <link rel="stylesheet" href="assets/css/custom.css">
+
+    <script src="assets/js/jquery-1.11.3.min.js"></script>
+</head>
+<?php
+session_start();
+if (isset($_SESSION['auth']) && $_SESSION['auth'] !== '') {
+     $requestPath = $_SERVER['REQUEST_URI'];
+     $role = $_SESSION['auth']['role_id'];
+    if ($role==2) {
+          /** Employee route */
+    } else {
+        /** employee route */
+        header("Location:admin/");
+        die();
+    }
+} else {
+    header("Location:../login.php");
+    die();
+}
+?>
---
+++ includes/top-navbar.php
@@ -0,0 +1,98 @@
+      <style>
+          .line {
+              display: inline-block;
+          }
+
+          .image.pull-right {
+              float: right;
+          }
+      </style>
+      <?php
+        include('./connection.php');
+        $pending_leaves_query1 = 'select * from employee_leaves where status = "Pending"';
+
+        function query1($conn, $sql)
+        {
+            $row2 = mysqli_query($conn, $sql);
+            return mysqli_num_rows($row2);
+        }
+        ?>
+      <div class="row">
+
+          <!-- Profile Info and Notifications -->
+          <div class="col-md-6 col-sm-8 clearfix">
+
+              <ul class="user-info pull-left pull-none-xsm">
+
+                  <!-- Profile Info -->
+                  <li class="profile-info dropdown"><!-- add class "pull-right" if you want to place this from right -->
+
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                          <img src="assets/images/thumb-1@2x.png" alt="" class="img-circle" width="44" />
+                          <?php echo $_SESSION['auth']['name']; ?>
+                      </a>
+
+                  </li>
+                  <li class="notifications dropdown">
+                      <?php
+                        $row2 = mysqli_query($conn, $pending_leaves_query1);
+
+                        ?>
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true" aria-expanded="false">
+                          <i class="entypo-bell"></i>
+
+                          <span class="badge badge-secondary"><?php echo mysqli_num_rows($row2);  ?></span>
+                      </a>
+                      <?php
+                        if (mysqli_num_rows($row2) > 0) {
+                            while ($record1 = mysqli_fetch_assoc($row2)) {
+                        ?>
+
+                              <ul class="dropdown-menu">
+                                  <li>
+
+                                      <ul class="dropdown-menu-list scroller" tabindex="5002" style="overflow: hidden; outline: none;">
+                                          <li>
+                                              <a href="#">
+                                                  <span class="line desc small">
+                                                      An Employess Want Leave
+                                                  </span>
+
+                                                  <span class="image pull-right">
+                                                      <?php $id1 = $record1['id']; ?>
+                                                      <a href="leave-detail.php?id=<?php echo $id1?>" class="btn btn-sm btn-info">View</a>
+                                                  </span>
+                                              </a>
+                                          </li>
+
+                                      </ul>
+                                  </li>
+
+
+                              </ul>
+                      <?php }
+                        } ?>
+
+                  </li>
+              </ul>
+
+          </div>
+
+
+          <!-- Raw Links -->
+          <div class="col-md-6 col-sm-4 clearfix hidden-xs">
+
+              <ul class="list-inline links-list pull-right">
+
+                  <li class="sep"></li>
+
+                  <li>
+                      <a href="./logout.php">
+                          Log Out <i class="entypo-logout right"></i>
+                      </a>
+                  </li>
+              </ul>
+
+          </div>
+
+      </div>
